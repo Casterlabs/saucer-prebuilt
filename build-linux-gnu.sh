8ce91933d33dd04265f4d0cbae8e188a1c4a761b
Ender <33337309+e3ndr@users.noreply.github.com>
2024-07-25 02:56:15 -0500

    xxxxxxx

--- build-linux-gnu.sh
+++ build-linux-gnu.sh
@@ -4,6 +4,8 @@
 set -e
 set -o pipefail
 
+export NONINTERACTIVE=1
+
 buildType=$1
 backend=$2
 arch=$3
@@ -15,13 +17,28 @@ elif [ $arch == "armhf" ]; then
 fi
 
 apt update
-apt install -y build-essential git gcc-14 cmake xvfb
+apt install -y curl build-essential git xvfb
 
 if [ $backend == "Qt5" ]; then
     apt install -y qtbase5-dev libqt5webchannel5-dev qtwebengine5-dev
 elif [ $backend == "Qt6" ]; then
     apt install -y qt6-base-dev qt6-declarative-dev qt6-svg-dev qt6-webchannel-dev qt6-webengine-dev
 fi
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(
+    echo
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+) >>/root/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install gcc@14
+brew install cmake
+
+echo "Building with $(gcc --version | grep 'gcc') and $(cmake --version | grep 'version')"
+
+export CC="/home/linuxbrew/.linuxbrew/bin/gcc-14"
+export CXX="/home/linuxbrew/.linuxbrew/bin/g++-14"
 
 cd saucer
 mkdir build
